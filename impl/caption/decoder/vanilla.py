3a98dafb1f7291d8c02b655862c91571111d5886
chenjia <marshimarocj@gmail.com>
2017-05-17 23:40:01 -0400

    sync

--- impl/caption/decoder/vanilla.py
+++ impl/caption/decoder/vanilla.py
@@ -128,9 +128,10 @@ class Decoder(base.DecoderBase):
         logit = tf.nn.xw_plus_b(outputs, self.softmax_W, self.softmax_B)
         logit_topk, word_topk = tf.nn.top_k(logit, k) # (batch_size, k)
         self._output_ops.append(word_topk)
-        self._beam_cum_logit_ops.append(logit_topk)
         self._beam_pre_ops.append(tf.range(0, batch_size*k))
-        end_idx = tf.where(word_topk == 1)
+        is_end = tf.equal(word_topk, tf.ones_like(word_topk, dtype=tf.int32))
+        end_idx = tf.where(is_end)
+        self._beam_cum_logit_ops.append(logit_topk)
         self._beam_end_ops.append(end_idx)
 
         wordids = framework.util.expanded_op.flatten(word_topk)
@@ -158,8 +159,9 @@ class Decoder(base.DecoderBase):
         self._beam_pre_ops.append(idx_topk//k)
 
         # set cumulated probability of completed sentences to -inf
-        logit_topk = tf.where(word_topk == 1, -100000000*tf.ones_like(logit_topk), logit_topk) 
-        end_idx = tf.where(word_topk == 1)
+        is_end = tf.equal(word_topk, tf.ones_like(word_topk, dtype=tf.int32))
+        logit_topk = tf.where(is_end, -100000000*tf.ones_like(logit_topk), logit_topk) 
+        end_idx = tf.where(is_end)
         self._beam_cum_logit_ops.append(logit_topk)
         self._beam_end_ops.append(end_idx)
 
@@ -273,7 +275,7 @@ class DecoderHiddenSet(base.DecoderBase):
         logit_topk, word_topk = tf.nn.top_k(logit, k) # (batch_size, k)
         self._output_ops.append(word_topk)
         self._beam_pre_ops.append(tf.range(0, batch_size*k))
-        is_end = tf.equal(word_topk, np.ones_like(word_topk, dtype=tf.int32))
+        is_end = tf.equal(word_topk, tf.ones_like(word_topk, dtype=tf.int32))
         logit_topk = tf.where(is_end, -100000000*tf.ones_like(logit_topk), logit_topk) 
         end_idx = tf.where(is_end)
         self._beam_cum_logit_ops.append(logit_topk)
@@ -306,7 +308,7 @@ class DecoderHiddenSet(base.DecoderBase):
         # print tmp.shape
 
         # set cumulated probability of completed sentences to -inf
-        is_end = tf.equal(word_topk, np.ones_like(word_topk, dtype=tf.int32))
+        is_end = tf.equal(word_topk, tf.ones_like(word_topk, dtype=tf.int32))
         logit_topk = tf.where(is_end, -100000000*tf.ones_like(logit_topk), logit_topk) 
         end_idx = tf.where(is_end)
         self._beam_cum_logit_ops.append(logit_topk)
