27d9cac9fe109975e35d0da8a9b5473ed5b20c14
chenjia <marshimarocj@gmail.com>
2017-05-17 23:59:28 -0400

    sync

--- impl/caption/model/base.py
+++ impl/caption/model/base.py
@@ -268,9 +268,11 @@ def predict_in_tst(trntst, sess, tst_reader, predict_file):
     sent_pool = framework.util.caption.utility.beamsearch_recover_captions(
       wordids, cum_logits, pres, ends, trntst.model_cfg.decoder_cfg.beam_width)
 
+    print len(sent_pool)
     for b in xrange(len(sent_pool)):
       videoid = str(tst_reader.videoids[b+base])
 
+      print sent_pool[b]
       trntst.output_by_sent_mode(sent_pool[b], videoid, videoid2caption)
 
     base += len(sent_pool)
