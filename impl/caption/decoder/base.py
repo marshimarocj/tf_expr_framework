2787d91a8fd42627f6569de96bcea7c293510d4d
chenjia <marshimarocj@gmail.com>
2017-05-19 23:59:52 -0400

    sync

--- impl/caption/decoder/base.py
+++ impl/caption/decoder/base.py
@@ -18,7 +18,7 @@ class ConfigBase(framework.model.proto.ProtoConfig):
     self.sent_pool_size = 5
 
     self.tied_embed = False
-    self.recurrent = False
+    self.variantional_recurrent = False
 
     self.reg_type = None
     self.reg_lambda = 0.
