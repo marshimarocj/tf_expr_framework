11f7ac6dd44cda0b190cf5ab8f48da0099dcbf45
Jia Chen <jiac@t470>
2017-10-01 18:33:31 -0400

    sync

--- model/module.py
+++ model/module.py
@@ -4,33 +4,61 @@ As you can see from the above description, this is a recursive process.
 Thus, both ModuleConfig and AbstractModule are designed to be recursive. 
 """
 import json
+import enum
 
 import tensorflow as tf
+
+
+class Mode(enum.Enum):
+  TRN_VAL = 0
+  TST = 1
 
 
 class ModuleConfig(object):
   """
   config of a module
   in addition to the customized parameters in the config, it contains tree special attributes:
-  [subconfigs] a dictionary of configs belong to the submodules in this module
+  [subcfgs] a dictionary of configs belong to the submodules in this module
   [freeze] boolean, whether to freeze the weights in this module in training. N.B. it doesn't affect the training of submodules
   [lr_mult] float, the multiplier to the base learning rate for weights in this modules. N.B. it doesn't affect the traninig of submodules
   [opt_alg] string, 'Adam|SGD|RMSProp', optimizer
   """
   def __init__(self):
-    self.subconfigs = {}
+    self.subcfgs = {}
     self.freeze = False
     self.lr_mult = 1.0
     self.opt_alg = 'Adam'
 
   def load(self, cfg_dict):
     for key in cfg_dict:
-      if key == 'subconfigs': # recursion
+      if key == 'subcfgs': # recursion
         data = cfg_dict[key]
         for key in data:
-          self.subconfigs[key].load(data[key])
+          self.subcfgs[key].load(data[key])
       elif key in self.__dict__:
         setattr(self, key, cfg_dict[key])
+
+    self._assert()
+
+  def save(self):
+    out = {}
+    for attr in self.__dict__:
+      if attr == 'subcfgs':
+        val = a.__dict__[attr]
+        out['subcfgs'] = {}
+        for key in val:
+          out['subcfgs'][key] = val.save()
+      else:
+        val = a.__dict__[attr]
+        if type(val) is not np.ndarray: # ignore nparray fields, which are used to initialize weights
+          out[attr] = self.__dict__[attr]
+    return out
+
+  def _assert(self):
+    """
+    check compatibility between configs
+    """
+    raise NotImplementedError("""please customize ModuleConfig._assert""")
 
 
 class AbstractModule(object):
@@ -42,16 +70,20 @@ class AbstractModule(object):
   in addition to the customized members, it contains three special members:
   [config] the config of this module
   [op2monitor] a dictionary of ops to monitor for debug
-  [submodules] a dictionary of submodules in this module
+  [submods] a dictionary of submodules in this module
   """
   name_scope = 'AbstractModule'
 
+  class InKey(enum.Enum):
+    pass
+
+  class OutKey(enum.Enum):
+    pass
+
   def __init__(self, config):
     self._config = config
-    self._submodules = {}
     self._op2monitor = {}
-    self._input_keys = []
-    self._output_keys = []
+    self._submods = self._set_submods()
 
   @property
   def config(self):
@@ -62,32 +94,38 @@ class AbstractModule(object):
     return self._op2monitor
 
   @property
-  def submodules(self):
-    return self._submodules
+  def submods(self):
+    return self._submods
 
-  def build_parameter_graph(self, basegraph):
+  def _set_submods(self):
     """
-    this would be called before get_out_ops_in_trn and get_out_ops_in_tst. 
+    return a dictionary of submods
+    """
+    raise NotImplementedError("""please customize AbstractModule._set_submods""")
+
+  def _build_parameter_graph(self, basegraph):
+    """
+    this would be called before get_out_ops_in_mode. 
     shared parts between trn and tst are encouraged to be placed in this function
     """
     raise NotImplementedError("""please customize AbstractModule.build_parameter_graph""")
 
-  def get_out_ops_in_trn(self, basegraph, in_ops):
-    """return out_ops (a dictionary) in trn given in_ops (a dictionary)"""
-    raise NotImplementedError("""please customize AbstractModule.get_out_ops_in_trn""")
+  def get_out_ops_in_mode(self, basegraph, in_ops):
+    """
+    return out_ops (a dictionary) given in_ops (a dictionary)
+    """
+    raise NotImplementedError("""please customize AbstractModule.get_out_ops_in_mode""")
 
-  def get_out_ops_in_val(self, basegraph, in_ops):
-    """return out_ops (a dictionary) in trn given in_ops (a dictionary)"""
-    raise NotImplementedError("""please customize AbstractModule.get_out_ops_in_trn""")
-
-  def get_out_ops_in_tst(self, basegraph, in_ops):
-    """return out_ops (a dictionary) in tst given in_ops (a dictionary)"""
-    raise NotImplementedError("""please customize AbstractModule.get_out_ops_in_trn""")
+  def build_parameter_graph(self, basegraph):
+    self._build_parameter_graph(basegraph)
+    for key in self._submods:
+      submod = self._submods[key]
+      submod.build_parameter_graph(basegraph)
 
 
-class ModelConfig(object):
+class ModelConfig(ModuleConfig):
   def __init__(self):
-    self.module_config = ModuleConfig()
+    ModuleConfig.__init__(self)
 
     self.trn_batch_size = 256
     self.tst_batch_size = 128
@@ -99,96 +137,65 @@ class ModelConfig(object):
   def load(self, file):
     with open(file) as f:
       data = json.load(f)
-      for key in data:
-      if key == 'module': # recursion
-        self.module_config.load(data[key])
-      elif key in self.__dict__:
-        setattr(self, key, data[key])
+      ModuleConfig.load(self, data)
+
+  def save(self, out_file):
+    out = ModuleConfig.save(self)
+    with open(out_file, 'w') as fout:
+      json.dump(out, fout, indent=2)
 
 
-class AbstractModel(object):
+class AbstractModel(AbstractModule):
   """
-  model contains the full computation graph, including loss, graident, save, summary in addition to inference
+  model is the root node in the tree of module composition and is a special type of module. 
+  therefore, it is an inheritance of AbstractModule. 
+  it contains the full computation graph, including loss, graident, save, summary in addition to inference
   a model has the following special members:
-  [_module] the module object, which is actually the root node of the module recursive tree
-  [trn_inputs] dictionary of trn input placeholder
-  [val_inputs] dictionary of val input placeholder
-  [tst_inputs] dictionary of tst input placeholder
-  [val_outputs] dictionary of val output ops
-  [tst_outputs] dictionary of tst output ops
   """
   name_scope = 'AbstractModel'
 
+  class DefaultKey(enum.Enum):
+    INIT = 'init'
+    LOSS = 'loss'
+    TRAIN = 'train'
+    SAVER = 'saver'
+    SUMMARY = 'summary'
+
   def __init__(self, config):
-    self.config = config
-    self._module = self._set_module()
+    AbstractModule.__init__(self, config)
 
-    self._trn_inputs = {}
-    self._val_inputs = {}
-    self._tst_inputs = {}
-    self._val_outputs = {}
-    self._tst_outputs = {}
+    self._inputs = {}
+    self._outputs = {}
 
-    self._loss_op = tf.no_op()
-    self._train_ops = []
-    self._saver = tf.no_op()
-    self._summary_op = tf.no_op()
-    self._init_op = tf.no_op()
-    self._op2monitor = {}
-
-  def _set_module(self):
-    """
-    return the module for the model
-    """
-    raise NotImplementedError("""please customize AbstractModel.set_module""")
-
-  def _add_trn_input(self):
+  def _add_input_in_mode(self, basegraph, mode):
     """
     return dictionary of input placeholders
     """
-    raise NotImplementedError("""please customize AbstractModel.set_trn_input""")
+    raise NotImplementedError("""please customize AbstractModel._add_input_in_mode""")
 
-  def _add_val_input(self):
-    """
-    return dictionary of input placeholders
-    """
-    raise NotImplementedError("""please customize AbstractModel.set_val_input""")
-
-  def _add_tst_input(self):
-    """
-    return dictionary of input placeholders
-    """
-    raise NotImplementedError("""please customize AbstractModel.set_tst_input""")
-
-  def _add_loss(self, trn_inputs, trn_outputs):
+  def _add_loss(self, basegraph):
     """
     return loss op
     """
-    raise NotImplementedError("""please customize AbstractModel.set_loss""")
+    raise NotImplementedError("""please customize AbstractModel._add_loss""")
+
+  def op_in_tst(self):
+    """
+    return dictionary of op in val
+    """
+    raise NotImplementedError("""please customize AbstractModel.op_in_tst""")
 
   @property
-  def trn_inputs(self):
-    return self._trn_inputs
+  def inputs(self):
+    return self._inputs
 
   @property
-  def val_inputs(self):
-    return self._val_inputs
-
-  @property
-  def tst_inputs(self):
-    return self._tst_inputs
-
-  @property
-  def val_outputs(self):
-    return self._val_outputs
-
-  @property
-  def tst_outputs(self):
-    return self._tst_outputs
+  def outputs(self):
+    return self._outputs
 
   @property
   def init_op(self):
-    return self._init_op
+    return self._outputs[self.DefaultKey.INIT]
 
   @property
   def saver(self):
@@ -198,73 +205,71 @@ class AbstractModel(object):
   def summary_op(self):
     return self._summary_op
 
-  @property
-  def op2monitor(self):
-      return self._op2monitor
-
-  def build_trn_val_graph(self):
+  def build_trn_tst_graph(self):
     basegraph = tf.Graph()
-    self._trn_inputs = self._add_trn_input()
-    self._val_inputs = self._add_val_input()
-    self._module.build_parameter_graph(basegraph)
-    trn_outputs = self._module.get_out_ops_in_trn(basegraph, self.trn_inputs)
-    self._val_outputs = self._module.get_out_ops_in_val(basegraph, self.val_inputs)
-    self._loss_op = self._add_loss(self.trn_inputs, trn_outputs)
-    self._calculate_gradient(basegraph)
+    self._inputs = self._add_input_in_mode(basegraph, Mode.TRN_VAL)
+    self.build_parameter_graph(basegraph)
+    self._outputs = self.get_out_ops_in_mode(basegraph, self._inputs, Mode.TRN_VAL)
+    self._outputs[self.DefaultKey.LOSS] = self._add_loss(basegraph)
+    self._outputs[self.DefaultKey.TRAIN] = self._calculate_gradient(basegraph)
 
     _recursive_gather_op2monitor_helper(self._module, self._op2monitor)
-    self._add_saver(basegraph)
-    self._add_summary(basegraph)
-    self._add_init(basegraph)
+    self._outputs[self.DefaultKey.SAVER] = self._add_saver(basegraph)
+    self._outputs[self.DefaultKey.SUMMARY] = self._add_summary(basegraph)
+    self._outputs[self.DefaultKey.INIT] = self._add_init(basegraph)
 
     return basegraph
 
-  def build_trn_tst_graph(self):
-    """
-    provided to make the inteface coherent with old version
-    this is actually building trn and val graph
-    """
-    self.build_trn_val_graph()
-
   def build_tst_graph(self):
     basegraph = tf.Graph()
-    self._tst_inputs = self._add_tst_input()
+    self._inputs = self._add_input_in_mode(basegraph, Mode.TST)
     self._module.build_parameter_graph(basegraph)
-    self._tst_outputs = self._module.get_out_ops_in_tst(basegraph, self.tst_inputs)
+    self._outputs = self.get_out_ops_in_mode(basegraph, self._inputs, Mode.TST)
 
-    self._add_saver(basegraph)
-    self._add_init(basegraph)
+    self._outputs[self.DefaultKey.SAVER] = self._add_saver(basegraph)
+    self._outputs[self.DefaultKey.INIT] = self._add_init(basegraph)
 
     return basegraph
 
   def op_in_trn(self):
     return {
-      'loss_op': self._loss_op, 
-      'train_ops': self._train_ops,
+      self.DefaultKey.LOSS: self._outputs[DefaultKey.LOSS],
+      self.DefaultKey.TRAIN: self._outputs[DefaultKey.TRAIN],
+    }
+
+  def op_in_val(self):
+    return {
+      self.DefaultKey.LOSS: self._outputs[self.DefaultKey.LOSS],
     }
 
   def _add_summary(self, basegraph):
     with basegraph.as_default():
       with tf.variable_scope(self.name_scope):
-        tf.summary.scalar('loss', self._loss_op)
+        tf.summary.scalar('loss', self._outputs[self.DefaultKey.LOSS])
         for var in tf.trainable_variables():
           tf.summary.histogram(var.name + '/activations', var)
-        self._summary_op = tf.summary.merge_all()
+        summary_op = tf.summary.merge_all()
+    return summary_op
 
   def _add_saver(self, basegraph):
     with basegraph.as_default():
-      self._saver = tf.train.Saver(tf.trainable_variables(), max_to_keep=1000)
+      saver = tf.train.Saver(tf.trainable_variables(), max_to_keep=1000)
+    return saver
 
   def _add_init(self, basegraph):
     with basegraph.as_default():
       with tf.variable_scope(self.name_scope):
-        self._init_op = tf.global_variables_initializer()
+        init = tf.global_variables_initializer()
+    return init
 
   def _calculate_gradient(self, basegraph):
+    train_ops = []
+    loss_op = self._outputs[self.DefaultKey.LOSS]
     with basegraph.as_default():
       with tf.variable_scope(self.name_scope):
-        _recursive_gradient_helper(self.module, self._loss_op, self.config.base_lr,
-          self._train_ops)
+        _recursive_gradient_helper(self, loss_op, self.config.base_lr,
+          train_ops)
+    return train_ops
 
 
 def _recursive_gradient_helper(module, loss_op, base_lr,
@@ -283,14 +288,14 @@ def _recursive_gradient_helper(module, loss_op, base_lr,
     grads_and_weights = optimizer.compute_gradients(loss_op, weight)
     train_ops.append(optimizer.apply_gradients(grads_and_weights))
   # recursive
-  for key in module.submodules:
-    submodule = module.submodules[key]
-    _recursive_gradient_helper(submodule, loss_op, base_lr, 
+  for key in module.submods:
+    submod = module.submods[key]
+    _recursive_gradient_helper(submod, loss_op, base_lr, 
       train_ops)
 
 
 def _recursive_gather_op2monitor_helper(module, op2monitor):
   op2monitor.update(module.op2monitor)
-  for key in module.submodules:
-    submodule = module.submodules[key]
-    _recursive_gather_op2monitor_helper(submodule, op2monitor)
+  for key in module.submods:
+    submod = module.submods[key]
+    _recursive_gather_op2monitor_helper(submod, op2monitor)
