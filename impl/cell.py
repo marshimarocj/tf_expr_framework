f24bda188a0cee29cb154033a0c1e76f5e8343ac
Jia Chen <jiac@t470>
2018-04-27 17:00:40 -0400

    sync

--- impl/cell.py
+++ impl/cell.py
@@ -178,3 +178,70 @@ class SCLSTMCell(framework.model.module.AbstractModule):
       self.OutKey.OUTPUT: output,
       self.OutKey.STATE: state,
     }
+
+
+class GRUCell(framework.model.module.AbstractModule):
+  name_scope = 'rnn.GRUCell'
+
+  class InKey(enum.Enum):
+    INPUT = 'input' # (None, dim_input)
+    STATE = 'state' # ((None, dim_hidden), (None, dim_hidden))
+    IS_TRN = 'is_training'
+
+  class OutKey(enum.Enum):
+    OUTPUT = 'output'
+    STATE = 'state'
+
+  @property
+  def state_size(self):
+    return self._config.dim_hidden
+
+  def _set_submods(self):
+    return {}
+
+  def _build_parameter_graph(self):
+    with tf.variable_scope(self.name_scope):
+      stddev = 1 / math.sqrt(self._config.dim_hidden + self._config.dim_input)
+      self.gate_W = tf.contrib.framework.model_variable('gate_W', 
+        shape=(self._config.dim_hidden + self._config.dim_input, 2*self._config.dim_hidden),
+        dtype=tf.float32,
+        initializer=tf.truncated_normal_initializer(stddev=stddev, dtype=tf.float32))
+      self.gate_b = tf.contrib.framework.model_variable('gate_b',
+        shape=(2*self._config.dim_hidden,),
+        dtype=tf.float32,
+        initializer=tf.constant_initializer(1.0, dtype=tf.float32))
+      self.candidate_W = tf.contrib.framework.model_variable('candidate_W',
+        shape=(self._config.dim_hidden + self._config.dim_input, self._config.dim_hidden),
+        dtype=tf.float32,
+        initializer=tf.truncated_normal_initializer(stddev=stddev, dtype=tf.float32))
+      self.candidate_b = tf.contrib.framework.model_variable('candidate_b',
+        shape=(self._config.dim_hidden,),
+        dtype=tf.float32,
+        initializer=tf.constant_initializer(0.0, dtype=tf.float32))
+      self._weights.append(self.gate_W)
+      self._weights.append(self.gate_b)
+      self._weights.append(self.candidate_W)
+      self._weights.append(self.candidate_b)
+
+  def _step(self, input, state, is_training):
+    input = tf.contrib.layers.dropout(input, keep_prob=self._config.keepin_prob, is_training=is_training)
+    gate_inputs = tf.nn.xw_plus_b(tf.concat([inputs, state], 1), self.gate_W, self.gate_b)
+    
+    value = tf.sigmoid(gate_inputs)
+    r, u = tf.split(value, num_or_size_splits=2, axis=1)
+
+    r_state = r * state
+
+    candidate = tf.nn.xw_plus_b(tf.concat([inputs, r_state], 1), self.candidate_W, self.candidate_B)
+
+    c = tf.tanh(candidate)
+    new_h = u * state + (1 - u) * c
+    return new_h
+
+  def get_out_ops_in_mode(self, in_ops, mode):
+    with tf.variable_scope(self.name_scope):
+      state = self._step(in_ops[self.InKey.INPUT], in_ops[self.InKey.STATE], in_ops[self.InKey.IS_TRN])
+    return {
+      self.OutKey.OUTPUT: state,
+      self.OutKey.STATE: state,
+    }
