b7859418603de4ff38f5a9bf3c233ea0161f5f4c
Jia Chen <jiac@t470>
2018-08-06 16:08:08 -0400

    sync

--- impl/encoder/pca.py
+++ impl/encoder/pca.py
@@ -21,7 +21,7 @@ class Encoder(framework.model.module.AbstractModule):
 
   class InKey(enum.Enum):
     FT = 'ft' # (None, dim_ft)
-    IS_TRN = 'is_training'
+    # IS_TRN = 'is_training'
 
   class OutKey(enum.Enum):
     EMBED = 'ft_embed' # (None, dim_output)
@@ -43,7 +43,7 @@ class Encoder(framework.model.module.AbstractModule):
 
   def _embed(self, in_ops):
     ft = in_ops[self.InKey.FT]
-    is_training = in_ops[self.InKey.IS_TRN]
+    # is_training = in_ops[self.InKey.IS_TRN]
 
     # ft = tf.contrib.layers.dropout(ft, keep_prob=self._config.keepin_prob, is_training=is_training)
     embed = tf.nn.xw_plus_b(ft, self.fc_W, self.fc_B)
