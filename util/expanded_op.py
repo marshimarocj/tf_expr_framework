e7d6a7decd926999e3d5db902af31bfd85cfbd44
Jia Chen <jiac@t470>
2018-08-06 21:50:05 -0400

    sync

--- util/expanded_op.py
+++ util/expanded_op.py
@@ -261,7 +261,7 @@ class LorentzGradientBuilder(object):
       lorentz_grad = x - updated_x
       # lorentz_grad = lorentz_grad / lr
       # lorentz_grad = tf.Print(lorentz_grad, [tf.norm(lorentz_grad, axis=-1), tf.norm(grad, axis=-1)], 'grad_norm')
-      lorentz_grad = lorentz_grad / tf.norm(lorentz_grad, axis=-1, keepdims=True) * tf.norm(grad, axis=-1, keepdims=True)
+      lorentz_grad = lorentz_grad / tf.norm(lorentz_grad, axis=-1, keep_dims=True) * tf.norm(grad, axis=-1, keep_dims=True)
       return [lorentz_grad]
 
     g = tf.get_default_graph()
